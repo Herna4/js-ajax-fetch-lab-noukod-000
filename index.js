d103e4c7b07e07e95f30ebfa06fc195da3c79e87
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-23 14:42:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const token = 'Herna4';
+
 function getIssues() {
 }
 
@@ -13,7 +15,29 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch('https://api.github.com/reposlearn-co-curriculum/javascript-fetch-lab/forks', {
+    method: 'post',
+    headers: {
+      Authorization: `token${getToken()}`
+    }
+    
+  })
+  .then(resp => resp.json())
+  .then(json => console.log(json));
 }
+// function forkRepo() {
+//   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+//   const url = 'https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks'
+//   fetch(url, {
+//   method: 'post',
+//     headers: {
+//       Accept: 'application/vnd.github.v3+json',
+//       Authorization: `token ${getToken()}`
+//     }
+//   })
+//   .then(res => res.json())
+//   .then(json => console.log(json));
+// }
 
 function getToken() {
   //change to your token to run in browser, but set
